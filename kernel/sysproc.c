45c237ff791d1c82fb7d740dc22d54c5aa350fdc
AnankaFishbein <48636000+AnankaFishbein@users.noreply.github.com>
2025-02-07 22:21:03 +0000

    restarting lazy alloc

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,9 +46,12 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  struct proc *p = myproc();
+  
+  addr = p->sz;
+  p->sz += n; // Increment process size for lazy allocation
+  printf("sys_sbrk: incrementing process size by %d, new size=%p\n", n, p->sz);
+
   return addr;
 }
 
